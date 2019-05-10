9047d4e697c6a0a1cf084f2f23f96cbfa84ee4c1
David McKinney (KAINOS) <d.mckinney@kainos.com>
2019-05-10 16:24:54 +0100

    2676: Add run junit script to enable sonarqube scanning of feature branch

--- health-tests/src/main/java/uk/gov/defra/tracesx/common/health/tests/RequestHelper.java
+++ health-tests/src/main/java/uk/gov/defra/tracesx/common/health/tests/RequestHelper.java
@@ -3,18 +3,19 @@ package uk.gov.defra.tracesx.common.health.tests;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class RequestHelper {
+final class RequestHelper {
 
-  private static final String SERVICE_BASE_URL = System.getProperty("service.base.url", "http://localhost:5460");
+  private static final String SERVICE_BASE_URL =
+      System.getProperty("service.base.url", "http://localhost:5460");
 
   private RequestHelper() {
   }
 
-  public static URL getUrl(String s) {
+  static URL getUrl(final String str) {
     try {
-      return new URL(SERVICE_BASE_URL + s);
-    } catch (MalformedURLException e) {
-      throw new RuntimeException(e);
+      return new URL(SERVICE_BASE_URL + str);
+    } catch (MalformedURLException ex) {
+      throw new RuntimeException(ex);
     }
   }
 }
