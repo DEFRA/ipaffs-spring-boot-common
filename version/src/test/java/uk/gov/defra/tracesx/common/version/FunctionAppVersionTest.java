40f854067588184843a4b9e9507437ed3315c9c7
lewis.birks <lewis.birks@defra.onmicrosoft.com>
2023-09-28 15:12:51 +0100

    fix: function app test can set env vars

--- version/src/test/java/uk/gov/defra/tracesx/common/version/FunctionAppVersionTest.java
+++ version/src/test/java/uk/gov/defra/tracesx/common/version/FunctionAppVersionTest.java
@@ -1,19 +1,18 @@
 package uk.gov.defra.tracesx.common.version;
 
-import static com.github.stefanbirkner.systemlambda.SystemLambda.withEnvironmentVariable;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.org.webcompere.systemstubs.SystemStubs.withEnvironmentVariable;
 
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class FunctionAppVersionTest {
@@ -23,21 +22,20 @@ class FunctionAppVersionTest {
   @Mock
   HttpResponseMessage.Builder httpResponseMessageBuilder;
 
-//  @Test
-//  void runVersion_ReturnsApplicationInformation() throws Exception {
-//    when(httpRequestMessage.createResponseBuilder(any())).thenReturn(httpResponseMessageBuilder);
-//    when(httpResponseMessageBuilder.header(any(), any())).thenReturn(httpResponseMessageBuilder);
-//    when(httpResponseMessageBuilder.body(any())).thenReturn(httpResponseMessageBuilder);
-//    withEnvironmentVariable("APP_NAME", "test_app")
-//        .and("API_VERSION", "1.2.3")
-//        .execute(
-//            () -> {
-//              new FunctionAppVersion().runVersion(httpRequestMessage);
-//            });
-//
-//    verify(httpRequestMessage).createResponseBuilder(HttpStatus.OK);
-//    verify(httpResponseMessageBuilder).header("Content-Type", "application/json");
-//    verify(httpResponseMessageBuilder)
-//        .body("{\"app\":{\"name\":\"test_app\",\"version\":\"1.2.3\"}}");
-//  }
+  @Test
+  void runVersion_ReturnsApplicationInformation() throws Exception {
+    when(httpRequestMessage.createResponseBuilder(any())).thenReturn(httpResponseMessageBuilder);
+    when(httpResponseMessageBuilder.header(any(), any())).thenReturn(httpResponseMessageBuilder);
+    when(httpResponseMessageBuilder.body(any())).thenReturn(httpResponseMessageBuilder);
+    withEnvironmentVariable("APP_NAME", "test_app")
+        .and("API_VERSION", "1.2.3")
+        .execute(() -> {
+          new FunctionAppVersion().runVersion(httpRequestMessage);
+        });
+
+    verify(httpRequestMessage).createResponseBuilder(HttpStatus.OK);
+    verify(httpResponseMessageBuilder).header("Content-Type", "application/json");
+    verify(httpResponseMessageBuilder)
+        .body("{\"app\":{\"name\":\"test_app\",\"version\":\"1.2.3\"}}");
+  }
 }
