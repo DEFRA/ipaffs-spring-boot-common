18b094264dab464e5bbd72890b0d3cf7ade40763
daniel.johansson <daniel.johansson@defra.onmicrosoft.com>
2025-05-30 12:47:54 +0100

    IMTA-19817: Made health checks for Azure and Jdbc overridable and bumped Spring Boot to 3.5.0

--- health/src/test/java/uk/gov/defra/tracesx/common/health/AzureHealthCheckTest.java
+++ health/src/test/java/uk/gov/defra/tracesx/common/health/AzureHealthCheckTest.java
@@ -21,10 +21,10 @@ import uk.gov.defra.tracesx.common.health.checks.AzureHealthCheck;
 @ExtendWith(MockitoExtension.class)
 class AzureHealthCheckTest {
 
-  private String serviceName = "imports-azure-search-shared";
-  private String indexName = "1-economic-operators-index";
-  private String apiKey = "292100772E2E949870ADA7F9038B1DBA";
-  private String apiVersion = "2017-11-11";
+  private static final String SERVICE_NAME = "imports-azure-search-shared";
+  private static final String INDEX_NAME = "1-economic-operators-index";
+  private static final String API_KEY = "292100772E2E949870ADA7F9038B1DBA";
+  private static final String API_VERSION = "2017-11-11";
 
   private String url;
 
@@ -32,43 +32,37 @@ class AzureHealthCheckTest {
 
   @BeforeEach
   public void setUp() {
-    url = buildAzureIndexSearchUrl(serviceName, indexName, apiVersion);
+    url = buildAzureIndexSearchUrl(SERVICE_NAME, INDEX_NAME, API_VERSION);
   }
 
   @Test
   void willReturnHealthyWhenAzureQueryOk() {
-
     when(restTemplate.exchange(
             eq(url), Mockito.eq(HttpMethod.POST), Mockito.any(), Mockito.<Class<String>>any()))
         .thenReturn(ResponseEntity.ok().build());
-    Health health =
-        new AzureHealthCheck(restTemplate, serviceName, indexName, apiKey, apiVersion).check();
+    Health health = new AzureHealthCheck(restTemplate, url, API_KEY).check();
 
     assertEquals(health, Health.up().build());
   }
 
   @Test
   void willReturnUnHealthyWhenAzureQueryNotOk() {
-
-    url = buildAzureIndexSearchUrl(serviceName, indexName, apiVersion);
+    url = buildAzureIndexSearchUrl(SERVICE_NAME, INDEX_NAME, API_VERSION);
     when(restTemplate.exchange(
             eq(url), Mockito.eq(HttpMethod.POST), Mockito.any(), Mockito.<Class<String>>any()))
         .thenReturn(ResponseEntity.notFound().build());
-    Health health =
-        new AzureHealthCheck(restTemplate, serviceName, indexName, apiKey, apiVersion).check();
+    Health health = new AzureHealthCheck(restTemplate, url, API_KEY).check();
 
     assertEquals(health, Health.down().build());
   }
 
   @Test
   void willReturnUnHealthyWhenClientThrowsException() {
-
-    url = buildAzureIndexSearchUrl(serviceName, indexName, apiVersion);
+    url = buildAzureIndexSearchUrl(SERVICE_NAME, INDEX_NAME, API_VERSION);
     when(restTemplate.exchange(
             eq(url), Mockito.eq(HttpMethod.POST), Mockito.any(), Mockito.<Class<String>>any()))
         .thenThrow(RestClientException.class);
-    Health health =
-        new AzureHealthCheck(restTemplate, serviceName, indexName, apiKey, apiVersion).check();
+    Health health = new AzureHealthCheck(restTemplate, url, API_KEY).check();
 
     assertEquals(health, Health.down().build());
   }
